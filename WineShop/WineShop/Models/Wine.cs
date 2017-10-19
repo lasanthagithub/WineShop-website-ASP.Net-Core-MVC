0662bc007548a110f2ab79167218d631ac8246b7
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-18 22:39:17 -0400

    Wine model class

---
+++ WineShop/WineShop/Models/Wine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WineShop.Models
+{
+    public class Wine
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int YearOfBotteling { get; set; }
+        public double AlcholPercentage { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+
+    }
+}
