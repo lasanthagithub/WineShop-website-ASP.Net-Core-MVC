8637aadca5501b9cf2ea4bd9a310d12102c9aa4c
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-18 22:52:07 -0400

    Winery model class

---
+++ WineShop/WineShop/Models/Winery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WineShop.Models
+{
+    public class Winery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Country Country { get; set; }
+
+        // Add relatioship
+        public virtual ICollection<Wine> Wines { get; set; }
+    }
+
+    public enum Country
+    {
+        France, Germany, Italy, Poland, India, UK, Netherlands, Other
+    }
+}
