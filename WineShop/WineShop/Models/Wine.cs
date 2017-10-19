694ae3303d6405f41a16252de2f544d88ff30429
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-18 23:58:32 -0400

    Wine class data annotation

--- WineShop/WineShop/Models/Wine.cs
+++ WineShop/WineShop/Models/Wine.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WineShop.Models
 {
     public class Wine
     {
+        [Key]
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage ="Name must be shorter than 25 characters")]
         public string Name { get; set; }
+
+        [Range(10, 200)]
         public double Price { get; set; }
+
+        [Range(1900, 2999)]
         public int YearOfBotteling { get; set; }
+
+        [Range(8.5, 22, ErrorMessage = "Alchol % should be between 8.5 to 22 %")]
         public double AlcholPercentage { get; set; }
+
         public string ImagePath { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+
         public WineType WineType { get; set; }
 
         // Creating a relationship - Many (wine) to one (Winery)
