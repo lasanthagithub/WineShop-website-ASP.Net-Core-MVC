6de3cdcf4d290874c6e60ce071ffdd5e71eaeedf
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-23 23:21:19 -0400

    Created DBinitializer class

---
+++ WineShop/WineShop/Data/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WineShop.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            // check for the database. If exist, nothing done. If not, create a new
+            context.Database.EnsureCreated();
+
+            // Look for data in Wine database
+            if (context.Wine.Any())
+            {
+                return; // DB has been seeded
+            }
+        }
+    }
+}
