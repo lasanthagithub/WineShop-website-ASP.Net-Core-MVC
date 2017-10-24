9717afd6e0c12cf1bb9ad362fbfbb72a51e30a6c
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-23 23:53:27 -0400

    Add instruction to main Method to run databases

--- WineShop/WineShop/Program.cs
+++ WineShop/WineShop/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using WineShop.Data;
 
 namespace WineShop
 {
@@ -14,7 +16,24 @@ namespace WineShop
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            //BuildWebHost(args).Run();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured while seeding the database");
+                }
+            }
+            host.Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
