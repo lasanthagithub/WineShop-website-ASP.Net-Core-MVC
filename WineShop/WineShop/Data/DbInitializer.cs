7ee04ba9ffb7e708ff6464b2f38b36a7f9830401
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-23 23:40:38 -0400

    Add some data to Wine

--- WineShop/WineShop/Data/DbInitializer.cs
+++ WineShop/WineShop/Data/DbInitializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WineShop.Models;
 
 namespace WineShop.Data
 {
@@ -14,6 +15,27 @@ namespace WineShop.Data
 
             // Look for data in Wine database
             if (context.Wine.Any())
+            {
+                return; // DB has been seeded
+            }
+
+            // Creating some data
+            var wine = new Wine[]
+            {
+                new Wine{Name = "JundaWine", Price = 19.99, YearOfBotteling = 1995, AlcholPercentage = 10, WineryID = 2},
+                new Wine{Name = "JundaNoWine", Price = 19.99, YearOfBotteling = 1990, AlcholPercentage = 10, WineryID = 1}
+            };
+
+            // Add new data to DB just for checking
+            foreach (Wine w in wine)
+            {
+                context.Wine.Add(w);
+            }
+
+
+
+            // Look for data in Winery database
+            if (context.Winery.Any())
             {
                 return; // DB has been seeded
             }
