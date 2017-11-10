be697efb4d03e9ec12d3f56e2b178fb83b7abbec
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-11-10 14:23:45 -0500

    HTML -index

--- WineShop/WineShop/Startup.cs
+++ WineShop/WineShop/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WineShop.Data;
 using Microsoft.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore.Extensions;
 
 namespace WineShop
 {
