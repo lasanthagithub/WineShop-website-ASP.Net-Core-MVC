b181bdd6f4e0c27de229568d4f4f387bdc72a18e
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-11-13 21:48:48 -0500

    WineriesController: Create(): HttpPost

--- WineShop/WineShop/Controllers/WineriesController.cs
+++ WineShop/WineShop/Controllers/WineriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WineShop.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,13 +11,26 @@ namespace WineShop.Controllers
 {
     public class WineriesController : Controller
     {
+
+        // Deal with databases
+
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
 
+        // Create get method
         public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Create post method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Winery winery)
         {
             return View();
         }
