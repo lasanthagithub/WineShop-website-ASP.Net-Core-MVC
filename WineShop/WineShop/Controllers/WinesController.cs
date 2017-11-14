7925bd3a58f32759439a8cb23193ea53c5c0b974
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-11-14 13:44:06 -0500

    WinesController: Sacffolding

---
+++ WineShop/WineShop/Controllers/WinesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WineShop.Data;
+using WineShop.Models;
+
+namespace WineShop.Controllers
+{
+    public class WinesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WinesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Wines
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Wine.Include(w => w.Winery);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Wines/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wine = await _context.Wine
+                .Include(w => w.Winery)
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (wine == null)
+            {
+                return NotFound();
+            }
+
+            return View(wine);
+        }
+
+        // GET: Wines/Create
+        public IActionResult Create()
+        {
+            ViewData["WineryID"] = new SelectList(_context.Winery, "Id", "Name");
+            return View();
+        }
+
+        // POST: Wines/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Price,YearOfBotteling,AlcholPercentage,ImagePath,Description,WineType,WineryID")] Wine wine)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(wine);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["WineryID"] = new SelectList(_context.Winery, "Id", "Name", wine.WineryID);
+            return View(wine);
+        }
+
+        // GET: Wines/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wine = await _context.Wine.SingleOrDefaultAsync(m => m.ID == id);
+            if (wine == null)
+            {
+                return NotFound();
+            }
+            ViewData["WineryID"] = new SelectList(_context.Winery, "Id", "Name", wine.WineryID);
+            return View(wine);
+        }
+
+        // POST: Wines/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Price,YearOfBotteling,AlcholPercentage,ImagePath,Description,WineType,WineryID")] Wine wine)
+        {
+            if (id != wine.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(wine);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WineExists(wine.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["WineryID"] = new SelectList(_context.Winery, "Id", "Name", wine.WineryID);
+            return View(wine);
+        }
+
+        // GET: Wines/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wine = await _context.Wine
+                .Include(w => w.Winery)
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (wine == null)
+            {
+                return NotFound();
+            }
+
+            return View(wine);
+        }
+
+        // POST: Wines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var wine = await _context.Wine.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Wine.Remove(wine);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WineExists(int id)
+        {
+            return _context.Wine.Any(e => e.ID == id);
+        }
+    }
+}
